ea7ff2ac0c9051d9e76c8f968856148b2b2fdd59
Lip-7 <zhighofficial@gmail.com>
2023-04-28 11:47:32 +0200

    init carusel of clients

--- src/components/MainInnerComponents/Clients/Client.vue
+++ src/components/MainInnerComponents/Clients/Client.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="wrap my-3">
         <div class="container">
-            <div class="row d-flex flex-nowrap overflow-hidden align-items-center">
-                <div class="col-12 col-sm-6 col-lg-3" v-for="company in store.main.clients">
-                    <SingleClient :client="company"/>
+            <div class="row d-flex flex-nowrap overflow-hidden align-items-center" @mousemove="dragging" @mousedown="() => this.isDragging = true" ref="clientsSlider">
+                <div class="col-12 col-sm-6 col-lg-3" v-for="company in store.main.clients" ref="clientsList">
+                    <SingleClient :client="company" ref="clientsItems"/>
                 </div>
             </div>
         </div>
@@ -18,17 +18,25 @@ export default {
     data() {
         return {
             store,
+            isDragging: false,
         }
     },
     methods: {
-
+        dragging(e) {
+            if (!this.isDragging) return;
+            this.$refs.clientsSlider.scrollLeft -= e.movementX;
+        },
+        dragStop() {
+            this.isDragging = false;
+        },
     },
     props:[],
     components: {
         SingleClient,
     },
     mounted() {
-
+        document.addEventListener('mouseup', this.dragStop)
+        console.log(this.$refs.clientsItems);
     }
 }
 </script>
--- src/components/MainInnerComponents/Clients/SingleClient.vue
+++ src/components/MainInnerComponents/Clients/SingleClient.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="wrapper w-100 h-100">
-        <div class="imgUp fullimg w-100 h-100">
-            <a href="#">
-                <img class="img-fluid" :src="client.img" :alt="client.name">
-            </a>
+    <div class="wrapper w-100 h-100 overflow-hidden position-relative">
+        <div class="imgcenter fullimg w-100 h-100">
+            <img draggable="false" class="img-fluid" :src="client.img" :alt="client.name"> 
+            <!-- <a href="">
+            </a> -->
+        </div>
+        <div draggable="false" class="imgover fullimg w-100 h-100">
+            <img class="img-fluid" :src="client.img" :alt="client.name">
+            <!-- <a href="">
+            </a> -->
         </div>
     </div>
 </template>
@@ -32,10 +37,20 @@ export default {
 <style lang="scss" scoped>
 @use '../../../assets/styles/partials/variables' as *;
 .wrapper{
+    cursor: pointer;
+    &:hover .fullimg{
+        animation: goDown .5s;
+    }
     .fullimg{
         a{
 
         }
+        transition: all .5s;
+
+    }
+    .imgover{
+        position: absolute;
+        top: -200px;
     }
 }
 </style>
